d677213b2f29353227fa6a239ca4e18fc987abf0
Ayush <28ayushsharma@gmail.com>
2019-01-03 21:46:27 +0530

    name change

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,11 +1,11 @@
 export const DISHES = [{
         id: 0,
-        name: 'Uthappizza',
+        name: 'pizza',
         image: 'assets/images/uthappizza.png',
         category: 'mains',
         label: 'Hot',
         price: '4.99',
-        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+        description: 'This is created by ayush sharma. A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
     },
     {
         id: 1,
